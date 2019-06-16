43c98e1f15b899147e6253ccf55ebf99db1e6c33
Noh seungyong <gadigun@gmail.com>
2019-06-16 15:27:03 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,34 @@
 
+struct card
+{
+    int index;
+    int used;
+}
+
+// 정돈된카드덱
+card [,] cardlist = new card[4,13];
+
+// 섞인 카드 덱
+card [,] resultCardlist = new card[4,13];
+
+// 카드 초기화
+for(int cardset = 0; cardset < 4; ++cardset)
+{
+    for(int i = 0; i < 13; ++i)
+    {
+        cardlist[cardset,i].index = i;
+        cardlist[cardset,i].used = 0;
+    }
+}
+
+// 카드 섞기
+for(int cardset = 0; cardset < 4; ++cardset)
+{
+    for(int i = 0; i < 13; ++i)
+    {
+        cardlist[cardset,i].index = i;
+        cardlist[cardset,i].used = 0;
+
+        resultCardlist[,].
+    }
+}
